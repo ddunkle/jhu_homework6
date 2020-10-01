61c3c7af74a2d84fb11d04c07126423b61eaa93b
Doug Dunkle <ddunkle@contourmtg.com>
2020-10-01 10:42:14 -0400

    Clean up, better READ me, better documentation

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,40 +1,47 @@
 $(document).ready(function() {
 
+// Declaration of global variables
 var apiKey = "4b9f79035e8d3dd47f7a2ebf1d60e057";
 var urlBase = "https://api.openweathermap.org/data/2.5/weather?q="
 var cities = [];
 init();
 
+// On search button click
 $("#citySearch").on("click", function () {
     var city = $("#city").val();
     var queryURL =  urlBase + city + "&appid=" + apiKey;
 
+    // Call to get the current weather
     getCurrentWeather(queryURL, city);    
 });
 
-function savedCityClick () {
-    var city = $(this).text();
-    var queryURL =  urlBase + city + "&appid=" + apiKey;
-    getCurrentWeather(queryURL, city); 
-}
-
+// Loading weather for a saved city
 $(".savedCity").on("click", function () {
+    
     var city = $(this).text();
     var queryURL =  urlBase + city + "&appid=" + apiKey;
-    getCurrentWeather(queryURL, city); 
+
+    // // Call to get the current weather
+     getCurrentWeather(queryURL, city); 
 });
 
+// Clearing out the saved cities list
 $("#clearCities").on("click", function () {
     localStorage.clear();
     cities = [];
     renderCities();
 })
 
+// Function call to populate the weather
 function getCurrentWeather(URL, city) {
+    // This API call gets the city by name. It returns the current weather and also the longitude/latitude
+    // needed in order to get the 5 day forecast. The current weather could also be gathered from the 5 day 
+    // and the actual weather icon comes from that api call. 
     $.ajax({
         url: URL,
         method: "GET"
     })
+    // Callback function. 
     .then(function(res) {
         console.log(res);
         $("#cityHolder").text(city +" "+ moment().format('l'));
@@ -44,26 +51,33 @@ function getCurrentWeather(URL, city) {
         $("#wind").text("Wind Speed: " + res.wind.speed + " MPH");
         var lat = (res.coord.lat);
         var lon = (res.coord.lon);
+        // Calling the function to get the 5 day forecast
         pop5Day(lat, lon);
+        // Checking to see if the city should be added to the left side nav
         addCity(city);
     })
     .catch(function(res){
+        // Alerts the user the city entered was not found in the API call
         alert((res.responseJSON.message));
     });
 };
+
+// Function and API calls to populate the 5 day forecast
 function pop5Day (lat, lon) {
     var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=" + apiKey
     $.ajax({
         url: queryURL,
         method: "GET"
     })
+    // Callback function to populate the response data
     .then(function(res) {
-        console.log(res);
+        // Completing the current day's weather
         $("#uv").text("UV Index: " + res.daily[0].uvi);
         $("#uvValue").text(res.daily[0].uvi);
         var icon = (res.daily[0].weather[0].icon)
         $("#curWeather").attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
-        console.log(res.daily[0].uvi);
+        
+        // Loop to populate each of the 5 daily cards
         for (i=0; i < 5; i++) {
             $("#" + i +"-h4").text(moment().add(i+1, 'days').format('l'));
             $("#" + i +"-img").attr("src", "");
@@ -76,9 +90,13 @@ function pop5Day (lat, lon) {
         }
     });
 };
+
+// Conversion of Kelvin to Fahrenheit
 function kelToFar(temp) {
     return Math.round((temp) * 9/5 - 459.67, 0)
 };
+
+// Function to render the cities to the left nav
 function renderCities () {
     $("#cityList").empty();
     for (i=0; i<cities.length; i++) {
@@ -86,10 +104,13 @@ function renderCities () {
         cityDiv.addClass("list-group-item savedCity");
         cityDiv.attr("value", cities[i]);
         cityDiv.text(cities[i]);
+        // needed to re-establish the on-click event listener
         cityDiv.click(savedCityClick);
         $("#cityList").prepend(cityDiv);
     };    
 };
+
+// Initializes the page pulling in the saved citys from local storage
 function init() {
   var storedCities = JSON.parse(localStorage.getItem("cities"));
 
@@ -97,10 +118,11 @@ function init() {
   if (storedCities !== null) {
     cities = storedCities;
   }
-
   // Render cities to the DOM
   renderCities();
 };
+
+// Looks to see if a city is in local storage
 function addCity(city) {
     var storedCities = JSON.parse(localStorage.getItem("cities"));
     for (i=0; i<cities.length; i++) {
@@ -112,5 +134,12 @@ function addCity(city) {
     localStorage.setItem("cities", JSON.stringify(cities));
     renderCities();
 };
+
+// Function for the event listener when clicking on a saved city.
+function savedCityClick () {
+    var city = $(this).text();
+    var queryURL =  urlBase + city + "&appid=" + apiKey;
+    getCurrentWeather(queryURL, city); 
+}
 
 })
--- README.md
+++ README.md
@@ -1,46 +1,21 @@
-# 06 Server-Side APIs: Weather Dashboard
+# jhu_homework6
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+## Purpose
+This project required a complete front end build along with api calls to the Open Weather API. It also relied upon the use of local storage. 
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+## Resources used
+Bootstrap and JQuery
+Open Weather API
 
-## User Story
+## Future goals
+Personally, I am really bad at styling and making something look good. If I had more time, I would try to make the site look better on a smaller device, as well as clean up the general visual appeal. I would also incorporate more of the data we get back from the API call, to better inform the user. 
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+Another stretch goal would be to allow the user to remove a single city from the saved city list. 
 
-## Acceptance Criteria
+## Link to live site.
+https://ddunkle.github.io/jhu_homework6/
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-WHEN I open the weather dashboard
-THEN I am presented with the last searched city forecast
-```
+## Images of deployed site.
 
-The following image demonstrates the application functionality:
+![Working Page](Assets/Images/Capture1.png)
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
